028dd162a6eb1ef3d06eeb22bb0195cf5b0a6db3
Ciznia <hosquetgabriel@gmail.com>
2025-10-06 17:02:15 +0200

    chore(nix): add a minimal flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, git-hooks }: let
+    inherit (nixpkgs) lib;
+
+    supportedSystems = [ "x86_64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs
+      supportedSystems (system: f nixpkgs.legacyPackages.${system});
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+
+    checks = forAllSystems (pkgs: {
+      pre-commit-check = git-hooks.lib.${pkgs.system}.run {
+        src = ./.;
+        hooks = lib.genAttrs [
+            "convco"
+            "trim-trailing-whitespace"
+            "deadnix"
+            "alejandra"
+          ] (_: {enable = true;});
+      };
+    });
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          docker
+          docker-compose
+
+          minikube
+          kubectl
+        ];
+      };
+    });
+  };
+}
