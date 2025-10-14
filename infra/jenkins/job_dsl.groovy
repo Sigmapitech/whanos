8736e2cda8664a3132ab32d25ae0e5c4719e90e7
Sigmanificient <edhyjox@gmail.com>
2025-10-14 15:40:57 +0200

    feat(jenkins): add jobs to build individual docker images

--- infra/jenkins/job_dsl.groovy
+++ infra/jenkins/job_dsl.groovy
@@ -1,9 +1,29 @@
 folder('Whanos base images') {
-    displayName('Whanos base images')
-    description('Base images of whanos.')
+  displayName('Whanos base images')
+  description('Base images of whanos.')
+
+  def languages = [
+    'befunge',
+    'c',
+    'python',
+    'java',
+    'javascript'
+  ]
+
+  languages.each { lang ->
+    freeStyleJob("Whanos base images/whanos-${lang}") {
+      steps {
+        shell("docker build -t whanos-${lang} - < /images/${lang}/Dockerfile.base")
+      }
+
+      triggers {
+        upstream('Build all base images')
+      }
+    }
+  }
 }
 
 folder('Projects') {
-    displayName('Projects')
-    description('Available projets in whanos')
+  displayName('Projects')
+  description('Available projets in whanos')
 }
