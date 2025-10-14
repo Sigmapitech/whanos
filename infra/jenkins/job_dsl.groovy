21dc1607cf3ab9a3b7fb72c93e6acd21be133ff6
Sigmanificient <edhyjox@gmail.com>
2025-10-14 03:49:26 +0200

    feat(jenkins): setup groovy & job folders

---
+++ infra/jenkins/job_dsl.groovy
@@ -0,0 +1,9 @@
+folder('Whanos base images') {
+    displayName('Whanos base images')
+    description('Base images of whanos.')
+}
+
+folder('Projects') {
+    displayName('Projects')
+    description('Available projets in whanos')
+}
