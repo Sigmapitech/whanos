587771f59cad1fb4cc38f9d6b66fa2d6e97a3805
Ciznia <hosquetgabriel@gmail.com>
2025-10-28 12:06:42 +0100

    style(jenkins): apply most linter hint

--- infra/jenkins/job_dsl.groovy
+++ infra/jenkins/job_dsl.groovy
@@ -1,47 +1,47 @@
 folder('Whanos base images') {
-  displayName('Whanos base images')
-  description('Base images of whanos.')
+    def List<String> languages = [
+        'befunge',
+        'c',
+        'python',
+        'java',
+        'javascript'
+    ]
 
-  def languages = [
-    'befunge',
-    'c',
-    'python',
-    'java',
-    'javascript'
-  ]
+    displayName(name)
+    description('Base images of whanos.')
 
-  languages.each { lang ->
-    freeStyleJob("Whanos base images/whanos-${lang}") {
-      steps {
-        shell("docker build -t whanos-foundation - < /images/Dockerfile.foundation")
-        shell("docker build -t whanos-${lang} - < /images/${lang}/Dockerfile.base")
-      }
+    languages.each { lang ->
+        freeStyleJob("${name}/whanos-${lang}") {
+            steps {
+                shell('docker build -t whanos-foundation - < /images/Dockerfile.foundation')
+                shell("docker build -t whanos-${lang} - < /images/${lang}/Dockerfile.base")
+            }
 
-      triggers {
-        upstream('Build all base images')
-      }
-    }
-  }
-
-  freeStyleJob("Whanos base images/Build all base images") {
-    steps {
-      shell('echo "Building all base images"')
-    }
-
-    publishers {
-      downstreamParameterized {
-        trigger(languages.collect { "Whanos base images/whanos-${it}" }) {
-          condition('ALWAYS')
-          parameters {
-            currentBuild()
-          }
+            triggers {
+                upstream('Build all base images')
+            }
         }
-      }
     }
-  }
+
+    freeStyleJob("${name}/Build all base images") {
+        steps {
+            shell('echo "Building all base images"')
+        }
+
+        publishers {
+            downstreamParameterized {
+                trigger(languages.collect { lang -> "${name}/whanos-${lang}" }) {
+                    condition('ALWAYS')
+                    parameters {
+                        currentBuild()
+                    }
+                }
+            }
+        }
+    }
 }
 
 folder('Projects') {
-  displayName('Projects')
-  description('Available projets in whanos')
+    displayName(name)
+    description('Available projects in whanos')
 }
